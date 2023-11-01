f714f406a539013f7da8c6d88faac2bd96003134
licheng <2501093733@qq.com>
2023-11-01 19:01:01 +0800

    初始化!

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
+    id("org.jetbrains.intellij") version "1.16.0"
+}
+
+val pluginVersion = "2.0.4"
+
+group = "com.thelumiereguy.compose_helper"
+version = pluginVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2023.2.4")
+    plugins.set(listOf("com.intellij.java", "Kotlin"))
+}
+
+kotlin {
+    jvmToolchain {
+        version = "17"
+    }
+}
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    withType<JavaCompile> {
+        targetCompatibility = "17"
+        sourceCompatibility = "17"
+    }
+
+    publishPlugin {
+        token.set(System.getenv("Jetbrains_Compose_Helper_Token"))
+    }
+
+
+    patchPluginXml {
+        version.set(pluginVersion)
+        sinceBuild.set("211.*")
+        untilBuild.set("")
+        changeNotes.set(
+            """
+           <ul>
+             <li><b>2.0.4</b> Added support for Android Studio - Giraffe and Hedgehog</li>
+             <li><b>2.0.3</b> Added support for Android Studio - Electric Eel and Flamingo</li>
+             <li><b>2.0.2</b> Added support for Android Studio - Chipmunk and Dolphin</li>
+             <li><b>2.0.1</b> Bug fix - Parent Composable not actually being removed in some cases</li>
+             <li><b>2.0.0</b> Complete Rewrite - Optimizations and added new features</li>
+             <li><b>1.0.2</b> Added support for older Android Studio versions</li>
+             <li><b>1.0.1</b> Added support for Artic Fox</li>
+             <li><b>1.0.0</b> Initial Version</li>
+          </ul>    
+            """.trimIndent()
+        )
+    }
+}
---
+++ src/main/java/com/thelumiereguy/compose_helper/intention/icons/SdkIcons.kt
@@ -0,0 +1,9 @@
+package com.thelumiereguy.compose_helper.intention.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object SdkIcons {
+
+    val composeIcon = IconLoader.getIcon("/icons/compose_icon.png", SdkIcons::class.java)
+
+}
