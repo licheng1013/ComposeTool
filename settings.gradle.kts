061fa06cd2f98f2096cdd68cac648c7c8ca4d751
licheng10113 <2501093733@qq.com>
2023-11-19 10:51:41 +0800

    1.0.1 版本发布 显示与资源目录下的导航icon关联

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "ComposeHelper"
+rootProject.name = "ComposeTool"
 
--- src/main/java/org/aiwan/compose/MyLineMarkerProvider.kt
+++ src/main/java/org/aiwan/compose/MyLineMarkerProvider.kt
@@ -3,11 +3,14 @@ package org.aiwan.compose
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import java.awt.Image
 import java.io.File
 import javax.swing.ImageIcon
@@ -20,50 +23,70 @@ class MyLineMarkerProvider : RelatedItemLineMarkerProvider() {
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
-        val file = element.containingFile.virtualFile ?: return
-        if (element !is KtProperty) {
+        this.stringResource(element,result)
+    }
+
+    private fun stringResource(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
+        val virtualFile = element.containingFile.virtualFile ?: return
+        if (element !is KtStringTemplateExpression) {
             return
         }
-        val match = Regex("\"(.*)\"").find(element.text) ?: return
-        val path = match.groupValues[1]
-        val indexOf = file.path.indexOf("kotlin")
-        if (indexOf == -1) {
-            return
-        }
-        // 截断后获取资源目录
-        val resourcesPath = file.path.subSequence(0, indexOf).toString() + "resources" + path
-        val resourcesFile  = File(resourcesPath)
-        if(!resourcesFile.exists()){
-            return
-        }
-        val fileName = resourcesFile.name
+        val file = getFile(virtualFile, element) ?: return
+
         var isImage = false
         for (it in fileSuffix) {
-            if (fileName.endsWith(it)) {
+            if (file.name.endsWith(it)) {
                 isImage = true
                 break
             }
         }
-        if(!isImage){
+        val type = PsiTreeUtil.findChildOfType(element, LeafPsiElement::class.java) ?: return
+        if (!isImage) {
+            result.add(iconOther.createLineMarkerInfo(type, MyGutterIconNavigationHandler(file.absolutePath)))
             return
         }
-
-        val type = PsiTreeUtil.findChildOfType(element, LeafPsiElement::class.java)
-        if (type != null) {
-            val builder = iconBuilder(resourcesPath)
-            result.add(builder.createLineMarkerInfo(type,MyGutterIconNavigationHandler(resourcesPath)))
-        }
+        val builder = iconImageBuilder(file.absolutePath)
+        result.add(builder.createLineMarkerInfo(type, MyGutterIconNavigationHandler((file.absolutePath))))
     }
+
+    private fun getFile(file: VirtualFile,element: PsiElement): File? {
+        val text = element.text
+
+        // 排除空字符串
+        if (text.replace("\"","").isBlank()) {
+            return null
+        }
+
+        val match = Regex("\"(.*)\"").find(text) ?: return null
+        val path = match.groupValues[1]
+        val indexOf = file.path.indexOf("kotlin")
+        if (indexOf == -1) {
+            return null
+        }
+        // 截断后获取资源目录
+        val resourcesPath = file.path.subSequence(0, indexOf).toString() + "resources" + path
+        val resourcesFile = File(resourcesPath)
+        if (!resourcesFile.exists()) {
+            return null
+        }
+        return resourcesFile
+    }
+
+
 
     private val fileSuffix = hashSetOf(".png", ".jpg", ".jpeg", ".gif", ".bmp")
 
-    private fun iconBuilder(path: String): NavigationGutterIconBuilder<PsiElement> {
+    private fun iconImageBuilder(path: String): NavigationGutterIconBuilder<PsiElement> {
         val originalIcon = ImageIcon(path)
         val originalImage: Image = originalIcon.image
         val resizedImage: Image = originalImage.getScaledInstance(16, 16, Image.SCALE_SMOOTH) // 设置新的宽度和高度
         val resizedIcon = ImageIcon(resizedImage)
 
         return NavigationGutterIconBuilder.create(resizedIcon)
-            .setAlignment(GutterIconRenderer.Alignment.LEFT).setTarget(null)
+            .setAlignment(GutterIconRenderer.Alignment.LEFT).setTarget(null).setTooltipText("Resource image")
     }
+
+
+    private val iconOther = NavigationGutterIconBuilder.create(AllIcons.Actions.ListFiles)
+    .setAlignment(GutterIconRenderer.Alignment.LEFT).setTarget(null).setTooltipText("Resource file")
 }
